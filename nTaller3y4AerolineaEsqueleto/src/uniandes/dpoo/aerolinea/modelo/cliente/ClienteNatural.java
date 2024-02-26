169eb94eaf949e9e799f4af15997524af48330c3
davmora04 <d.morar@uniandes.edu.co>
2024-02-25 19:42:10 -0500

    Cambios finales

---
+++ nTaller3y4AerolineaEsqueleto/src/uniandes/dpoo/aerolinea/modelo/cliente/ClienteNatural.java
@@ -0,0 +1,21 @@
+package uniandes.dpoo.aerolinea.modelo.cliente;
+
+public class ClienteNatural {
+
+	public ClienteNatural(String nombre) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static final String NATURAL = null;
+
+	public String getIdentificador() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTipoCliente() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
